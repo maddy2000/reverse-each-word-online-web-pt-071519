55b31a1142e07e43a43dc0e1e588285f30830818
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-04 23:58:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   sentence1.split(/ /)
- puts sentence1.map do |sentence|
+ puts sentence1.each do |sentence|+reverse!
  # >> sentence1.reverse! do |sentence1|
 end
 end
