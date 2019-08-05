180cf7ecf9aded1ecacc58ac935e71edae7e0479
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-05 00:27:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   sentence1.split(/ /)
-  result== "Hello", "there,", "and", "how", "are", "you?"
+  result= 
   result.each do |result|
   result.reverse do |result|
 end
